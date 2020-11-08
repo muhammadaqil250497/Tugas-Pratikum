9ffd007511acfa1310808bb5f445dffc98663b0b
muhammadaqil250497 <muhammadaqil250497@gmail.com>
2020-11-08 13:57:04 +0700

    Update

--- Modul 7 Latihan 1.py
+++ Modul 7 Latihan 1.py
@@ -1,53 +1,9 @@
-print("Modul 7. Latihan 1")
+baris = 10
+kolom = baris
 
-s =' '	
-for i in range(0,10):
-	s += str(i)+' '
-print (s)
-
-s =' '
-for i in range(1,11):
-	s += str(i)+' '
-print (s)
-
-s =' '	
-for i in range(2,12):
-	s += str(i)+' '
-print (s)
-
-s =' '	
-for i in range(3,13):
-	s += str(i)+' '
-print (s)
-
-s =' '	
-for i in range(4,14):
-	s += str(i)+' '
-print (s)
-
-s =' '
-for i in range(5,15):
-	s += str(i)+' '
-print (s)
-
-s =' '
-for i in range(6,16):
-	s += str(i)+' '
-print (s)
-
-s =' '
-for i in range(7,17):
-	s += str(i)+' '
-print (s)
-
-s =' '
-for i in range(8,18):
-	s += str(i)+' '
-print (s)
-
-s =' '
-for i in range(9,19):
-	s += str(i)+' '
-print (s)
-
-print("*****SELESAI*****")
+for bar in range(baris):
+    for col in range(kolom):
+        tab = bar+col
+        print("{0:>5}".format(tab), end='')
+    print()
+    
